783c83b2a5aae347f8cda3371bd903358aefe4a6
Chris Yoon <cyoon@pnimedia.com>
2014-12-16 22:11:15 -0800

    Create devices controller, model and views

---
+++ DeviceTrackerWeb/DAL/DeviceTrackerWebContext.cs
@@ -0,0 +1,21 @@
+﻿using DeviceTrackerWeb.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DeviceTrackerWeb.DAL
+{
+    public class DeviceTrackerWebContext : DbContext
+    {
+        public DeviceTrackerWebContext()
+            : base("DeviceTrackerWebContext")
+        {
+        }
+        
+        public DbSet<Device> Devices { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ DeviceTrackerWeb/Models/Device.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DeviceTrackerWeb.Models
+{
+    public class Device
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string DeviceId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Model { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Made { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string OS { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ScreenSize { get; set; }
+
+        [StringLength(50)]
+        public string User { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CheckOutTime { get; set; }
+    }
+}
