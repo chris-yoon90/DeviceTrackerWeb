521f652125edb75e541eebff3a31b5cbff8f1a58
Richard Hoksbergen <rhoksbergen@pnimedia.com>
2015-01-06 13:07:34 -0800

    Added reservation model

---
+++ DeviceTrackerWeb/Models/Reservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeviceTrackerWeb.Models
+{
+    public class Reservation
+    {
+        public int ID { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd H:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime ReservationCheckOutTime { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd H:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime ReservationCheckInTime { get; set; }
+
+        public virtual DTIdentityUser DTIdentityUser { get; set; }
+
+        public virtual Device Device { get; set; }
+    }
+}
